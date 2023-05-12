7479961b1e4957e7713ef14939af46030017b72a
labombabird <133056585+labombabird@users.noreply.github.com>
2023-05-12 01:45:42 -0600

    agrege el programa

---
+++ Nueva carpeta/Multiplos.java
@@ -0,0 +1,17 @@
+
+
+package com.mycompany.multiplos;
+
+public class Multiplos {
+
+    public static void main(String[] args) {
+        for(int o=0; o<=100; o++){
+        
+   if(o%3==0){
+        System.out.println(o+ "el multiplo es Try");
+    }else if (o%5==0){
+        System.out.println(o+"El multiplo es Fiv");
+    }
+        }
+    }
+}
