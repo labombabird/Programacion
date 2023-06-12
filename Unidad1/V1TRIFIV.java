c4fbe8355e12fd26664caf22bd55e1b308d3bde0
labombabird <133056585+labombabird@users.noreply.github.com>
2023-06-12 09:21:55 -0600

    Add files via upload
    Se suben todos los archivos

---
+++ Unidad1/V1TRIFIV.java
@@ -0,0 +1,38 @@
+
+
+package com.mycompany.v1trifiv;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class V1TRIFIV {
+
+    public static void main(String[] args) throws IOException {
+        //declaracion de datos del usuario
+        int numero1;
+        int multa3;
+        int multa5;
+        BufferedReader bufEntrada= new BufferedReader(new InputStreamReader(System.in));
+   
+        //entrada de datos
+        System.out.println("dame el numero");
+        numero1= Integer.parseInt(bufEntrada.readLine());
+        
+        //Proceso de salida
+        multa3= numero1%3;
+        multa5= numero1%5;
+        //salida de datos
+        if(multa3==0){ System.out.println("Try");
+        
+        }if(multa5==0){ System.out.println("Five");
+        
+        }if(multa3==0 && multa5== 0){ System.out.println("try five");
+        }
+        
+
+    }
+    }
+    
+       
+        
+        
+  
